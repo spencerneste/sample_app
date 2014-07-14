fa7c1b5f07c59878e25bc31aa99f81239181ac54
Spencer Neste <spencerneste@yahoo.com>
2014-07-13 23:44:10 -0700

    SSL Security

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,9 @@ SampleApp::Application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+    config.force_ssl = true
+
+
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
