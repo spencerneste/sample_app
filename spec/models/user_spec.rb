1691fa4599305f5eeb15ed83a8c84141f357d3e5
Spencer Neste <spencerneste@yahoo.com>
2014-07-14 15:12:05 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
   it { should respond_to(:authenticate)}
+  it { should respond_to(:remember_token) }
 
 
   it {should be_valid}
@@ -22,6 +23,11 @@ describe User do
   	before { @user.name = "a" * 51}
   	it {should_not be_valid}
   	 end
+
+     describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
  describe "return value of authenticate method " do 
  	before {@user.save}
